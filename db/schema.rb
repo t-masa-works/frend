4089fe9118ba2382ae82c8e41c985877253f931d
tobe masa <t.masa.works@gmail.com>
2023-07-31 11:08:02 +0900

    テーブル作成と紐付け

---
+++ db/schema.rb
@@ -0,0 +1,24 @@
+
+
+ActiveRecord::Schema.define(version: 2023_07_31_020346) do
+
+  enable_extension "plpgsql"
+
+  create_table "pictures", force: :cascade do |t|
+    t.text "image"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_pictures_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  add_foreign_key "pictures", "users"
+end
