214e4c38226df5da761a548be46707bade2cbb1b
tobe masa <t.masa.works@gmail.com>
2023-08-01 09:23:25 +0900

    ログアウト処理実装

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,4 +12,10 @@ class SessionsController < ApplicationController
       render :new
     end
   end
+  
+  def destroy
+    session.delete(:user_id)
+    flash[:notice] = 'ログアウトしました'
+    redirect_to new_session_path
+  end
 end
