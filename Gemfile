6f796aa64b6252cdf6d4eab2de41413520da1c6e
tobe masa <t.masa.works@gmail.com>
2023-08-01 13:43:34 +0900

    notice修正

--- Gemfile
+++ Gemfile
@@ -21,7 +21,7 @@ end
 group :development do
   gem 'web-console', '>= 4.1.0'
 
-  gem 'rack-mini-profiler', '~> 2.0'
+  # gem 'rack-mini-profiler', '~> 2.0'
   gem 'listen', '~> 3.3'
   gem 'spring'
 end
--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -21,7 +21,7 @@ class PicturesController < ApplicationController
 
     respond_to do |format|
       if @picture.save
-        format.html { redirect_to picture_url(@picture), notice: "Picture was successfully created." }
+        format.html { redirect_to picture_url(@picture), notice: "画像の保存に成功しました" }
         format.json { render :show, status: :created, location: @picture }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -35,7 +35,7 @@ class PicturesController < ApplicationController
 
     respond_to do |format|
       if @picture.update(picture_params)
-        format.html { redirect_to picture_url(@picture), notice: "Picture was successfully updated." }
+        format.html { redirect_to picture_url(@picture), notice: "画像の編集に成功しました." }
         format.json { render :show, status: :ok, location: @picture }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -49,7 +49,7 @@ class PicturesController < ApplicationController
     @picture.destroy
 
     respond_to do |format|
-      format.html { redirect_to pictures_url, notice: "Picture was successfully destroyed." }
+      format.html { redirect_to pictures_url, notice: "画像を削除しました." }
       format.json { head :no_content }
     end
   end
