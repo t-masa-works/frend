d72bcc370c53b19bc1f1ba2210e8bfb09b219a32
tobe masa <t.masa.works@gmail.com>
2023-08-01 14:05:59 +0900

    ログイン者のみのページ制限

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   include SessionsHelper
+  before_action :login_required
+
+  private
+
+  def login_required
+    redirect_to new_session_path unless current_user
+  end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,5 @@
 class SessionsController < ApplicationController
+  skip_before_action :login_required, only: [:new, :create]
   def new
   end
 
@@ -12,7 +13,7 @@ class SessionsController < ApplicationController
       render :new
     end
   end
-  
+
   def destroy
     session.delete(:user_id)
     flash[:notice] = 'ログアウトしました'
