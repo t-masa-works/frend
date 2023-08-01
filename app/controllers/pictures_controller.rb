56cf28c9483173d73a65f59b5ae37af893475c51
tobe masa <t.masa.works@gmail.com>
2023-08-01 09:50:59 +0900

    ログイン時の画像投稿機能実装

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -17,6 +17,7 @@ class PicturesController < ApplicationController
 
   def create
     @picture = Picture.new(picture_params)
+    @picture.user = current_user
 
     respond_to do |format|
       if @picture.save
