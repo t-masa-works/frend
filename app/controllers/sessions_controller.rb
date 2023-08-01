f14a144b596e5f7b6f611f28a2f6d42b074b6f9f
tobe masa <t.masa.works@gmail.com>
2023-08-01 09:13:34 +0900

    サインアップやログイン、アウトボタン設置

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,8 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      redirect_to user_path(user.id)
     else
       flash.now[:danger] = 'ログインに失敗しました'
       render :new
